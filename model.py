7fdcb72d3e07f25b4343480addb3ad3d000d3796
Lynn Potter <mlpotter124@gmail.com>
2021-05-02 09:53:04 -0700

    Add Gru4Rec with Feature
    * Added function to initialize weight embedding of movie plots with BERT
    features
    * Added projection function to make the plot embedding same size as movie
    embedding
    * updated the forward function

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence
 from torch.nn.utils.rnn import pad_packed_sequence
 import torch.nn.functional as F
+import torch
 
 class DilatedResBlock(nn.Module):
     def __init__(self,dilation,channel,max_len):
@@ -120,3 +121,66 @@ class gru4rec(nn.Module):
         x = self.output_layer(x)
                 
         return x
+    
+class gru4recF(nn.Module):
+    """
+    embedding dim: the dimension of the item-embedding look-up table
+    hidden_dim: the dimension of the hidden state of the GRU-RNN
+    batch_first: whether the batch dimension should be the first dimension of input to GRU-RNN
+    output_dim: the output dimension of the last fully connected layer
+    max_length: the maximum session length for any user, used for packing/padding input to GRU-RNN
+    pad_token: the value that pad tokens should be set to for GRU-RNN and item embedding
+    bert_dim: the dimension of the feature-embedding look-up table
+    """
+    def __init__(self,embedding_dim,hidden_dim,output_dim,batch_first=True,max_length=200,pad_token=0,bert_dim=768):
+        super(gru4recF,self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim =hidden_dim
+        self.batch_first =batch_first
+        self.output_dim =output_dim
+        self.max_length = 200
+        self.pad_token = pad_token
+        self.bert_dim = bert_dim
+    
+        # initialize item-id lookup table
+        # add 1 to output dimension because we have to add a pad token
+        self.movie_embedding = nn.Embedding(output_dim+1,embedding_dim,padding_idx=pad_token)
+        
+        #  initialize plot lookup table
+        # add 1 to output dimensino because we have to add a pad token
+        self.plot_embedding = nn.Embedding(output_dim+1,bert_dim,padding_idx=pad_token)
+        self.plot_embedding.requires_grad_(requires_grad=False)
+        
+        # project plot embedding to same dimensionality as movie embedding
+        self.plot_projection = nn.Linear(bert_dim,embedding_dim)
+
+
+        self.encoder_layer = nn.GRU(embedding_dim,self.hidden_dim,batch_first=self.batch_first)
+
+        # add 1 to the output dimension because we have to add a pad token
+        self.output_layer = nn.Linear(hidden_dim,output_dim+1)
+    
+    def forward(self,x,x_lens,pack=True):
+        # add the plot embedding and movie embedding
+        # do I add non-linearity or not? ... 
+        # concatenate or not? ...
+        # many questions ...
+        x = self.movie_embedding(x) + self.plot_projection(self.plot_embedding(x))
+        
+                    
+        if pack:
+            x = pack_padded_sequence(x,x_lens,batch_first=True,enforce_sorted=False)
+        
+        output_packed,_ = self.encoder_layer(x)        
+        
+        if pack:
+            x, _ = pad_packed_sequence(output_packed, batch_first=self.batch_first,total_length=self.max_length,padding_value=self.pad_token)
+            
+        x = self.output_layer(x)
+                
+        return x
+    
+    def init_weight(self,reset_object,feature_embed):
+        for (item_id,embedding) in feature_embed.items():
+            item_id = reset_object.item_enc.inverse_transform([item_id]).item()
+            self.plot_embedding.weight.data[item_id,:] = torch.DoubleTensor(embedding)
