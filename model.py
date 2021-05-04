da008ce2f6497ad0042cc201bef152ca11a57dfc
Lynn Potter <mlpotter124@gmail.com>
2021-05-03 19:00:38 -0700

    Add genre capabilites + Merge models
    * Merge old model and new model
    * May now handle any combination of genre embeddings, plot embeddings,
    item embeddings, or any such combination.

--- model.py
+++ model.py
@@ -70,7 +70,7 @@ class NextItNet(nn.Module):
         
         self.hidden_layers = nn.Sequential(*[nn.Sequential(*[DilatedResBlock(d,embedding_dim,max_len) for d in dilations]) for _ in range(hidden_layers)])
 
-        self.final_layer = nn.Linear(embedding_dim, output_dim+1)
+        self.final_layer = nn.Linear(embedding_dim, output_dim)
 
     
     def forward(self,x):
@@ -78,48 +78,6 @@ class NextItNet(nn.Module):
         x = self.hidden_layers(x)
         x = self.final_layer(x.permute(0,2,1))
         
-        return x
-    
-class gru4rec(nn.Module):
-    """
-    embedding dim: the dimension of the item-embedding look-up table
-    hidden_dim: the dimension of the hidden state of the GRU-RNN
-    batch_first: whether the batch dimension should be the first dimension of input to GRU-RNN
-    output_dim: the output dimension of the last fully connected layer
-    max_length: the maximum session length for any user, used for packing/padding input to GRU-RNN
-    pad_token: the value that pad tokens should be set to for GRU-RNN and item embedding
-    """
-    def __init__(self,embedding_dim,hidden_dim,output_dim,batch_first=True,max_length=200,pad_token=0):
-        super(gru4rec,self).__init__()
-        self.embedding_dim = embedding_dim
-        self.hidden_dim =hidden_dim
-        self.batch_first =batch_first
-        self.output_dim =output_dim
-        self.max_length = max_length
-        self.pad_token = pad_token
-    
-        # initialize item-id lookup table
-        # add 1 to output dimension because we have to add a pad token
-        self.movie_embedding = nn.Embedding(output_dim+1,embedding_dim,padding_idx=pad_token)
-
-        self.encoder_layer = nn.GRU(embedding_dim,self.hidden_dim,batch_first=self.batch_first)
-
-        # add 1 to the output dimension because we have to add a pad token
-        self.output_layer = nn.Linear(hidden_dim,output_dim+1)
-    
-    def forward(self,x,x_lens,pack=True):
-        x = self.movie_embedding(x)
-                    
-        if pack:
-            x = pack_padded_sequence(x,x_lens,batch_first=True,enforce_sorted=False)
-        
-        output_packed,_ = self.encoder_layer(x)        
-        
-        if pack:
-            x, _ = pad_packed_sequence(output_packed, batch_first=self.batch_first,total_length=self.max_length,padding_value=self.pad_token)
-            
-        x = self.output_layer(x)
-                
         return x
     
 class gru4recF(nn.Module):
@@ -131,19 +89,22 @@ class gru4recF(nn.Module):
     max_length: the maximum session length for any user, used for packing/padding input to GRU-RNN
     pad_token: the value that pad tokens should be set to for GRU-RNN and item embedding
     bert_dim: the dimension of the feature-embedding look-up table
+    ... to do add all comments ... 
     """
-    def __init__(self,embedding_dim,hidden_dim,output_dim,genre_dim=1,batch_first=True,max_length=200,pad_token=0,pad_genre_token=0,bert_dim=768,genre=False):
+    def __init__(self,embedding_dim,hidden_dim,output_dim,genre_dim=0,batch_first=True,max_length=200,pad_token=0,pad_genre_token=0,bert_dim=0):
         super(gru4recF,self).__init__()
+        
+        self.batch_first =batch_first
+        
         self.embedding_dim = embedding_dim
         self.hidden_dim =hidden_dim
-        self.batch_first =batch_first
         self.output_dim =output_dim
         self.genre_dim = genre_dim
+        self.bert_dim = bert_dim
+
         self.max_length = max_length
         self.pad_token = pad_token
         self.pad_genre_token = pad_genre_token
-        self.bert_dim = bert_dim
-        self.genre = genre
     
         # initialize item-id lookup table
         # add 1 to output dimension because we have to add a pad token
@@ -151,31 +112,35 @@ class gru4recF(nn.Module):
         
         #  initialize plot lookup table
         # add 1 to output dimensino because we have to add a pad token
-        self.plot_embedding = nn.Embedding(output_dim+1,bert_dim,padding_idx=pad_token)
-        self.plot_embedding.requires_grad_(requires_grad=False)
+        if bert_dim != 0:
+            self.plot_embedding = nn.Embedding(output_dim+1,bert_dim,padding_idx=pad_token)
+            self.plot_embedding.requires_grad_(requires_grad=False)
+            
+            # project plot embedding to same dimensionality as movie embedding
+            self.plot_projection = nn.Linear(bert_dim,embedding_dim)
         
-        # project plot embedding to same dimensionality as movie embedding
-        self.plot_projection = nn.Linear(bert_dim,embedding_dim)
-        
-        if genre:
-            self.genre_embedding = nn.Embedding(genre_dim,embedding_dim,padding_idx=pad_genre_token)
+        if genre_dim != 0:
+            self.genre_embedding = nn.Embedding(genre_dim+1,embedding_dim,padding_idx=pad_genre_token)
 
 
         self.encoder_layer = nn.GRU(embedding_dim,self.hidden_dim,batch_first=self.batch_first)
 
         # add 1 to the output dimension because we have to add a pad token
-        self.output_layer = nn.Linear(hidden_dim,output_dim+1)
+        self.output_layer = nn.Linear(hidden_dim,output_dim)
     
-    def forward(self,x,x_lens,pack=True):
+    def forward(self,x,x_lens,x_genre=None,pack=True):
         # add the plot embedding and movie embedding
         # do I add non-linearity or not? ... 
         # concatenate or not? ...
         # many questions ...
-        if self.genre:
-            x = self.movie_embedding(x) + self.plot_projection(self.plot_embedding(x)) + self.genre_embedding(x).sum(1)
-        else:
+        if (self.bert_dim != 0) and (self.genre_dim != 0):
+            x = self.movie_embedding(x) + self.plot_projection(self.plot_embedding(x)) + self.genre_embedding(x_genre).sum(2)
+        elif (self.bert_dim != 0) and (self.genre_dim == 0):
             x = self.movie_embedding(x) + self.plot_projection(self.plot_embedding(x)) 
-        
+        elif (self.bert_dim == 0) and (self.genre_dim != 0):
+            x = self.movie_embedding(x) + self.genre_embedding(x_genre).sum(2)
+        else:
+            x = self.movie_embedding(x)
                     
         if pack:
             x = pack_padded_sequence(x,x_lens,batch_first=True,enforce_sorted=False)
