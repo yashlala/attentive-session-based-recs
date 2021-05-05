16957e5e5e8a55e193b7256200002b3d1be48bfb
Lynn Potter <mlpotter124@gmail.com>
2021-05-04 18:01:21 -0700

    Added gru4rec feature only model

--- model.py
+++ model.py
@@ -246,13 +246,6 @@ class gru4recFC(nn.Module):
             
 class gru4rec_vanilla(nn.Module):
     """
-    embedding dim: the dimension of the item-embedding look-up table
-    hidden_dim: the dimension of the hidden state of the GRU-RNN
-    batch_first: whether the batch dimension should be the first dimension of input to GRU-RNN
-    output_dim: the output dimension of the last fully connected layer
-    max_length: the maximum session length for any user, used for packing/padding input to GRU-RNN
-    pad_token: the value that pad tokens should be set to for GRU-RNN and item embedding
-    bert_dim: the dimension of the feature-embedding look-up table
     ... to do add all comments ... 
     """
     def __init__(self,hidden_dim,output_dim,batch_first=True,max_length=200,pad_token=0):
@@ -299,3 +292,67 @@ class gru4rec_vanilla(nn.Module):
         x = self.output_layer(x)
                 
         return x
+    
+class gru4rec_feature(nn.Module):
+    """
+    embedding dim: the dimension of the item-embedding look-up table
+    hidden_dim: the dimension of the hidden state of the GRU-RNN
+    batch_first: whether the batch dimension should be the first dimension of input to GRU-RNN
+    output_dim: the output dimension of the last fully connected layer
+    max_length: the maximum session length for any user, used for packing/padding input to GRU-RNN
+    pad_token: the value that pad tokens should be set to for GRU-RNN and item embedding
+    bert_dim: the dimension of the feature-embedding look-up table
+    ... to do add all comments ... 
+    """
+    def __init__(self,hidden_dim,output_dim,batch_first=True,max_length=200,pad_token=0,bert_dim=0):
+        super(gru4rec_feature,self).__init__()
+        
+        self.batch_first =batch_first
+        
+        self.hidden_dim =hidden_dim
+        self.output_dim =output_dim
+
+        self.max_length = max_length
+        self.pad_token = pad_token
+        
+        self.genre_dim = 0
+        self.bert_dim = 0
+    
+        # initialize item-id lookup table as one hot vector
+        self.plot_embedding = nn.Embedding(output_dim+1,bert_dim,padding_idx=pad_token)
+        self.plot_embedding.requires_grad_(requires_grad=False)
+        
+        #  initialize plot lookup table
+        # add 1 to output dimensino because we have to add a pad token
+
+        self.encoder_layer = nn.GRU(bert_dim,self.hidden_dim,batch_first=self.batch_first)
+
+        # add 1 to the output dimension because we have to add a pad token
+        self.output_layer = nn.Linear(hidden_dim,output_dim)
+    
+    def forward(self,x,x_lens,x_genre=None,pack=True):
+        # add the plot embedding and movie embedding
+        # do I add non-linearity or not? ... 
+        # concatenate or not? ...
+        # many questions ...
+
+        x = self.plot_embedding(x)
+                    
+        if pack:
+            x = pack_padded_sequence(x,x_lens,batch_first=True,enforce_sorted=False)
+        
+        output_packed,_ = self.encoder_layer(x)        
+        
+        if pack:
+            x, _ = pad_packed_sequence(output_packed, batch_first=self.batch_first,total_length=self.max_length,padding_value=self.pad_token)
+            
+        x = self.output_layer(x)
+                
+        return x
+    
+    def init_weight(self,reset_object,feature_embed):
+        for (item_id,embedding) in feature_embed.items():
+            if item_id not in reset_object.item_enc.classes_:
+                continue
+            item_id = reset_object.item_enc.transform([item_id]).item()
+            self.plot_embedding.weight.data[item_id,:] = torch.DoubleTensor(embedding)
