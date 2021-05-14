63382ed437f82ba24095e8510ae1d80188a58e92
Yash Lala <yashlala@gmail.com>
2021-05-14 00:01:48 -0700

    Add additive (no dot-product) attention to `attention.py`.

---
+++ attention/attention.py
@@ -0,0 +1,51 @@
+"""This module contains attention function definitions."""
+
+# We want to define an attention function.
+# Our tutorial used additive multiplication. We know that this's much slower
+# than dot-product style multiplication -- but it's also more accurate and/or
+# less prone to blowup? We should use the same attention function as the
+# GRU4REC paper, anyways, so let's implement dot-product style too.
+#
+# TODO: write GRU4REC dot-product style attention metric.
+
+class AdditiveAttention(nn.Module):
+    """Implements Additive (MLP) attention"""
+
+    def __init__(self, hidden_size, key_size=None, query_size=None):
+        super(AdditiveAttention, self).__init__()
+
+        # We assume a bi-directional encoder so key_size is 2*hidden_size
+        key_size = 2 * hidden_size if key_size is None else key_size
+        query_size = hidden_size if query_size is None else query_size
+
+        self.key_layer = nn.Linear(key_size, hidden_size, bias=False)
+        self.query_layer = nn.Linear(query_size, hidden_size, bias=False)
+        self.energy_layer = nn.Linear(hidden_size, 1, bias=False)
+
+        # to store attention scores
+        self.alphas = None
+
+    def forward(self, query=None, proj_key=None, value=None, mask=None):
+        assert mask is not None, "mask is required"
+
+        # We first project the query (the decoder state).
+        # The projected keys (the encoder states) were already pre-computated.
+        query = self.query_layer(query)
+
+        # Calculate scores.
+        scores = self.energy_layer(torch.tanh(query + proj_key))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        # Mask out invalid positions.
+        # The mask marks valid positions so we invert it using `mask & 0`.
+        scores.data.masked_fill_(mask == 0, -float('inf'))
+
+        # Turn scores to probabilities.
+        alphas = F.softmax(scores, dim=-1)
+        self.alphas = alphas
+
+        # The context vector is the weighted sum of the values.
+        context = torch.bmm(alphas, value)
+
+        # context shape: [B, 1, 2D], alphas shape: [B, 1, M]
+        return context, alphas
--- attention/encoder_decoder.py
+++ attention/encoder_decoder.py
@@ -1,4 +1,4 @@
-"""A set of wrapper modules to be shaped into attentive GRU4REC."""
+"""A set of attentive encoder + decoder related modules."""
 
 
 # ========================
@@ -151,59 +151,3 @@ class Decoder(nn.Module):
             return None  # start with zeros
 
         return torch.tanh(self.bridge(encoder_final))
-
-
-
-# ========================
-# MODEL UTILITIES
-# ========================
-
-# We want to define an attention function.
-# This tutorial used additive multiplication. We know that it's much slower
-# than dot-product style multiplication -- but it's also more accurate and/or
-# less prone to blowup? Find out later. We should write the same attention
-# function as the GRU4REC paper, anyways.
-#
-# TODO: write GRU4REC Dot-Product style attention metric.
-
-class AdditiveAttention(nn.Module):
-    """Implements Additive (MLP) attention"""
-
-    def __init__(self, hidden_size, key_size=None, query_size=None):
-        super(AdditiveAttention, self).__init__()
-
-        # We assume a bi-directional encoder so key_size is 2*hidden_size
-        key_size = 2 * hidden_size if key_size is None else key_size
-        query_size = hidden_size if query_size is None else query_size
-
-        self.key_layer = nn.Linear(key_size, hidden_size, bias=False)
-        self.query_layer = nn.Linear(query_size, hidden_size, bias=False)
-        self.energy_layer = nn.Linear(hidden_size, 1, bias=False)
-
-        # to store attention scores
-        self.alphas = None
-
-    def forward(self, query=None, proj_key=None, value=None, mask=None):
-        assert mask is not None, "mask is required"
-
-        # We first project the query (the decoder state).
-        # The projected keys (the encoder states) were already pre-computated.
-        query = self.query_layer(query)
-
-        # Calculate scores.
-        scores = self.energy_layer(torch.tanh(query + proj_key))
-        scores = scores.squeeze(2).unsqueeze(1)
-
-        # Mask out invalid positions.
-        # The mask marks valid positions so we invert it using `mask & 0`.
-        scores.data.masked_fill_(mask == 0, -float('inf'))
-
-        # Turn scores to probabilities.
-        alphas = F.softmax(scores, dim=-1)
-        self.alphas = alphas
-
-        # The context vector is the weighted sum of the values.
-        context = torch.bmm(alphas, value)
-
-        # context shape: [B, 1, 2D], alphas shape: [B, 1, M]
-        return context, alphas
